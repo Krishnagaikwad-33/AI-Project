bfe624f63617daeba092cac8b38efa727978e167
krishna <24kgaikwad33@gmail.com>
2025-07-23 06:20:30 +0000

    corrected the metric namespace to pull the memory metrics

--- metrics.py
+++ metrics.py
@@ -5,30 +5,54 @@ def get_metrics(instance_id, region):
     cloudwatch = boto3.client("cloudwatch", region_name=region)
     ec2 = boto3.client("ec2", region_name=region)
 
+    # Last 30 days
     end_time = datetime.utcnow()
-    start_time = end_time - timedelta(hours=6)
+    start_time = end_time - timedelta(days=30)
 
     def fetch_metric(metric_name, unit="Percent"):
+        if metric_name == "MemoryUtilization":
+            # For memory, use CWAgent namespace and proper metric name
+            actual_metric = "mem_used_percent"
+            namespace = "CWAgent"
+            dimensions = [
+                {"Name": "InstanceId", "Value": instance_id},
+                {"Name": "ImageId", "Value": get_image_id(instance_id, region)},
+                {"Name": "InstanceType", "Value": get_instance_type(instance_id, region)}
+            ]
+        else:
+            # For CPU
+            actual_metric = metric_name
+            namespace = "AWS/EC2"
+            dimensions = [{"Name": "InstanceId", "Value": instance_id}]
+        
         response = cloudwatch.get_metric_statistics(
-            Namespace="CWAgent" if metric_name == "MemoryUtilization" else "AWS/EC2",
-            MetricName=metric_name,
-            Dimensions=[
-                {"Name": "InstanceId", "Value": instance_id}
-            ],
+            Namespace=namespace,
+            MetricName=actual_metric,
+            Dimensions=dimensions,
             StartTime=start_time,
             EndTime=end_time,
-            Period=300,
+            Period=3600,
             Statistics=["Average", "Maximum"],
             Unit=unit
         )
-        # Sort by timestamp
         return sorted(response["Datapoints"], key=lambda x: x["Timestamp"])
 
-    # Get instance metadata
+    def get_image_id(instance_id, region):
+        # Required for CWAgent dimensions
+        ec2 = boto3.client('ec2', region_name=region)
+        instance = ec2.describe_instances(InstanceIds=[instance_id])
+        return instance['Reservations'][0]['Instances'][0]['ImageId']
+
+    def get_instance_type(instance_id, region):
+        # Required for CWAgent dimensions
+        ec2 = boto3.client('ec2', region_name=region)
+        instance = ec2.describe_instances(InstanceIds=[instance_id])
+        return instance['Reservations'][0]['Instances'][0]['InstanceType']
+
+    # Get instance details
     instance_desc = ec2.describe_instances(InstanceIds=[instance_id])
     instance_info = instance_desc["Reservations"][0]["Instances"][0]
-    
-    # Extract Name tag and private IP
+
     name_tag = next(
         (tag["Value"] for tag in instance_info.get("Tags", []) if tag["Key"] == "Name"),
         "Unnamed"
